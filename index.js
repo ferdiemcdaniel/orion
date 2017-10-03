e3ddea6195cfcc613d5fd678ac42f6c450a1383c
Ferdinand McDaniel <imcdanielamano@gmail.com>
2017-10-03 17:46:39 +0800

    [Caching] added dataloaders as the context caching of requests in graphql. Added some dataloaders in the users entities when querying a relationship

--- index.js
+++ index.js
@@ -2,30 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {graphqlExpress, graphiqlExpress} = require('apollo-server-express');
 const {authenticate} = require('./authentication');
-
 const schema = require('./schema');
-
 const connectMongo = require('./mongo-connector');
+const buildDataloaders = require('./dataloaders');
+
 const start = async () => {
     const mongo = await connectMongo();
     const buildOptions = async (req, res) => {
         const user = await authenticate(req, mongo.Users);
         return {
-            context: {mongo, user},
+            context: {
+                dataloaders: buildDataloaders(mongo),
+                mongo,
+                user,
+            },
             schema,
         };
     };
     let app = express();
-    app.use(
-        '/graphql',
-        bodyParser.json(),
-        graphqlExpress(buildOptions)
-    );
+    app.use('/graphql', bodyParser.json(), graphqlExpress(buildOptions));
     app.use(
         '/graphiql',
         graphiqlExpress({
             endpointURL: '/graphql',
-            passHeader: `'Authorization': 'bearer token-ferdinand.amano@goabroad.com'`, 
+            passHeader: `'Authorization': 'bearer token-ferdinand.amano@goabroad.com'`,
         })
     );
     const PORT = 3000;
